cf31641bab20adbfe34792a33543d4d300687fd3
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-02-24 14:16:33 +0900

    モーダルのコンテンツ押下時の背景変化

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ $(function(){
   $('.tweetButton').click(function () { 
     $(this).find('i').toggleClass('checkcircle-color-checked');
   });
-  $('.testbox').click(function () { 
+  $('label').click(function () { 
     $(this).find('i').toggleClass("checkcircle-color-checked");
-    $(this).toggleClass('testbox-checked');
+    $(this).parent().toggleClass('testbox-checked');
   });
 
 
