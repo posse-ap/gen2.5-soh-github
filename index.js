bba3da26f6869a14550a0daf2a4d09f9740c4182
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-02-27 12:10:43 +0900

    円グラフ一個目途中

--- index.js
+++ index.js
@@ -20,4 +20,39 @@ $(function(){
     $('.modalWindow').width(w);
     $('.modalWindow').height(h);
   })
+
+  // Load Charts and the corechart package.
+  google.charts.load('current', {'packages':['corechart']});
+
+  // Draw the chart when Charts is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Draw the pie chart for the Anthony's pizza when Charts is loaded.
+  // google.charts.setOnLoadCallback(drawAnthonyChart);
+
+  // Callback that draws the chart.
+  function drawChart() {
+
+    // Create the data table
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'コンテンツ');
+    data.addColumn('number', '割合');
+    data.addRows([
+      ["N予備校", 40],
+      ["ドットインストール", 20],
+      ["課題", 40]
+    ]);
+
+    // Set options for chart.
+    var options = {
+      pieHole: 0.5,
+      colors: ['#2245EC','#2371BD','#39BDDE'],
+      chartArea:{width:'100%',height:'100%'},
+      legend:{position:'none'}
+    };
+
+    // Instantiate and draw the chart.
+    var chart = new google.visualization.PieChart(document.getElementById('contents-chart'));
+    chart.draw(data, options);
+  }
 })
