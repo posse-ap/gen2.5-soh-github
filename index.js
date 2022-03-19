64d66c197b463a641940d20ad4a3b3df6540251c
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-03-19 11:14:46 +0900

    棒グラフデータの読み込み変更

--- index.js
+++ index.js
@@ -172,37 +172,7 @@ function loading(){
 
         // Create the data table
         let data = new google.visualization.arrayToDataTable([
-          ['day','time'],
-          arr[0],
-          arr[1],
-          arr[2],
-          arr[3],
-          arr[4],
-          arr[5],
-          arr[6],
-          arr[7],
-          arr[8],
-          arr[9],
-          arr[10],
-          arr[11],
-          arr[12],
-          arr[13],
-          arr[14],
-          arr[15],
-          arr[16],
-          arr[17],
-          arr[18],
-          arr[19],
-          arr[20],
-          arr[21],
-          arr[22],
-          arr[23],
-          arr[24],
-          arr[25],
-          arr[26],
-          arr[27],
-          arr[28],
-          arr[29],
+          ['day','time'],...arr
         ]);
 
         // Set options for chart.
