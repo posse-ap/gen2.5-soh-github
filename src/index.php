43add8ec3fecb52bc1201a1401904d48348f6cdd
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-07-03 18:22:08 +0900

    ymlファイルでmysqlコンテナを立てるようにした

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Kuizy</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
   <div class="wrapper">
@@ -15,6 +15,6 @@
   </div>
   
   
-  <script src="../kuizy.js"></script>
+  <script src="./kuizy.js"></script>
 </body>
 </html>
