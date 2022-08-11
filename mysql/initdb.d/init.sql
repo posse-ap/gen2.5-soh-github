0765d9f6aed8edcb0422b518ad0710bd39c6fd2c
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-08-11 17:20:21 +0900

    テーブルを2つ追加

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS quizy_db;
 CREATE DATABASE quizy_db;
 USE quizy_db;
 
+-- BIG_QUESTIONS
 DROP TABLE IF EXISTS big_questions;
 CREATE TABLE big_questions (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -9,3 +10,36 @@ CREATE TABLE big_questions (
 ) CHARSET=utf8;
 
 INSERT INTO big_questions(pref_name) VALUES ('東京'), ('広島');
+
+-- QUESTIONS
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  big_question_id INT,
+  img VARCHAR(140)
+) CHARSET=utf8;
+
+INSERT INTO questions(big_question_id, img) VALUES 
+    (1, '../img/takanawa.png'),
+    (1, '../img/kameido.png'),
+    (2, '../img/mukainada.png');
+
+-- CHOICES
+DROP TABLE IF EXISTS choices;
+CREATE TABLE choices (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  question_id INT,
+  name VARCHAR(140),
+  valid INT
+) CHARSET=utf8;
+
+INSERT INTO choices(question_id, name, valid) VALUES
+    (1, 'たかなわ', 1),
+    (1, 'たかわ', 0),
+    (1, 'こうわ', 0),
+    (2, 'かめいど', 1),
+    (2, 'かめと', 0),
+    (2, 'かめど', 0),
+    (3, 'むかいなだ', 1),
+    (3, 'むこうひら', 0),
+    (3, 'むきひら', 0);
