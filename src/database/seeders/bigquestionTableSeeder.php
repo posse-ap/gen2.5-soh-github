57d76be8d5d2faf2e4170ebcf2ae593db219e104
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-12-13 22:03:32 +0900

    bigquestionsテーブル作成＆seederでデータ登録済

---
+++ src/database/seeders/bigquestionTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class bigquestionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'pref_name' => '東京',
+        ];
+        $param = [
+            'pref_name' => '広島',
+        ];
+        DB::table('bigquestions')->insert($param);
+    }
+}
