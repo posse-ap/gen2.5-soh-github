d85c802332161812b8bf003452f42acacab91893
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2023-01-22 15:05:42 +0900

    ログイン失敗時の処理完了

--- src/app/Http/Requests/Auth/LoginRequest.php
+++ src/app/Http/Requests/Auth/LoginRequest.php
@@ -46,7 +46,7 @@ class LoginRequest extends FormRequest
         $this->ensureIsNotRateLimited();
 
         if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
+            RateLimiter::hit($this->throttleKey(), 180);
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
