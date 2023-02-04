1681d9581620c9b1308afd1b37dfd457b6c9f06e
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2023-02-04 16:05:05 +0900

    2/4横モク

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -2,37 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-    body {font-size: 16pt; color:#999;}
-    h1 {font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt){
-    return "<{$tag}>".$txt."</{$tag}>";
-}
-
 class HelloController extends Controller
 {
-    // public function index(Request $request, Response $response) {
-    //     global $head, $style, $body, $end;
-    //     $html = $head . tag('title','Hello/Index') . $style . 
-    //         $body
-    //         . tag('h1', 'Index') . tag('h3', 'Request') . '<pre>'. $request .'</pre>' . tag('h3', 'Response') . '<pre>'. $response .'</pre>'
-    //         . $end;
-    //     $response->setContent($html);
-    //     return $response;
-    // }
     public function index() {
-        return view('hello.index', ['msg'=>'']);
+        $items = Models\BigQuestion::all();
+
+        return view('quizy.quizytop', ['prefecture' => $items]);
     }
 
     public function post(Request $request){
---
+++ src/resources/views/quizy/quizytop.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quizy　TOP</title>
+  <link rel="stylesheet" href="/css/reset.css">
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <div class="wrapper">
+    <li>東京</li>
+    <li>広島</li>
+    @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+  </div>
+</body>
+</html>
